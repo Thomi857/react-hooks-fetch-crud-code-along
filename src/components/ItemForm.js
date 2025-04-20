62f7b0f7b48e3bb823d1413950e25e40d49424f9
CliffLinusthomi <linusthomi2@gmail.com>
2025-04-20 23:24:19 +0300

    fetchng data

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = {
+      name: name,
+      category: category,
+      isInCart: false
+    };
+
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((r) => r.json())
+      .then((newItemFromServer) => {
+        // ✅ Call the callback prop with the returned item
+        onAddItem(newItemFromServer);
+
+        // Optional: reset the form
+        setName("");
+        setCategory("Produce");
+      });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
